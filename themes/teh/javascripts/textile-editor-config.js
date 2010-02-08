e23a978ffcbfd11085b4d571d5de6d924fffb7cb
Scott Boms <sboms@Gato.local>
2010-02-08 02:22:53 +0000

    Made H5 and H6 buttons visible in the editor.

--- themes/teh/javascripts/textile-editor-config.js
+++ themes/teh/javascripts/textile-editor-config.js
@@ -16,6 +16,8 @@ teButtons.push(new TextileEditorButton('ed_h1', 'h1.png', 'h1', '\n', '1', 'Head
 teButtons.push(new TextileEditorButton('ed_h2', 'h2.png', 'h2', '\n', '2', 'Header 2'));
 teButtons.push(new TextileEditorButton('ed_h3', 'h3.png', 'h3', '\n', '3', 'Header 3'));
 teButtons.push(new TextileEditorButton('ed_h4', 'h4.png', 'h4', '\n', '4', 'Header 4'));
+teButtons.push(new TextileEditorButton('ed_h5', 'h5.png', 'h5', '\n', '5', 'Header 5'));
+teButtons.push(new TextileEditorButton('ed_h6', 'h6.png', 'h6', '\n', '6', 'Header 6'));
 teButtons.push(new TextileEditorButton('ed_block', 'blockquote.png', 'bq', '\n', 'q', 'Blockquote'));
 teButtons.push(new TextileEditorButton('ed_outdent', 'outdent.png', ')', '\n', ']', 'Outdent'));
 teButtons.push(new TextileEditorButton('ed_indent', 'indent.png', '(', '\n', '[', 'Indent'));
